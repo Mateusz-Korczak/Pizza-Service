aedc50b9465f6a5d38a5a049c5f0858de808855c
Mateusz Korczak <korczak.programowanie@gmail.com>
2021-11-01 11:41:56 +0100

    fixed prepareCartProductParams functionallity and solved task adding products into shopping cart

--- src/js/script.js
+++ src/js/script.js
@@ -234,27 +234,34 @@
 
     prepareCartProductParams() {
       const thisProduct = this;
+
       const formData = utils.serializeFormToObject(thisProduct.form);
       const params = {};
 
+      // for very category (param)
       for (let paramId in thisProduct.data.params) {
         const param = thisProduct.data.params[paramId];
 
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
         params[paramId] = {
           label: param.label,
           options: {},
         };
 
+        // for every option in this category
         for (let optionId in param.options) {
           const option = param.options[optionId];
           const optionSelected =
             formData[paramId] && formData[paramId].includes(optionId);
 
           if (optionSelected) {
-            params[paramId].options = option;
+            params[paramId].options = {
+              [optionId]: option.label,
+            };
           }
         }
       }
+
       return params;
     }
   }
@@ -342,7 +349,9 @@
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(
         select.cart.toggleTrigger
       );
-      console.log(thisCart.dom.toggleTrigger);
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(
+        select.cart.productList
+      );
     }
 
     initActions() {
@@ -354,8 +363,17 @@
     }
 
     add(menuProduct) {
-      // const thisCart = this;
+      const thisCart = this;
+      console.log('thisCart: ', this);
       console.log('adding product', menuProduct);
+
+      const generatedHTML = templates.cartProduct(menuProduct);
+      console.log(generatedHTML);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      console.log(generatedDOM);
+      thisCart.dom.productList.appendChild(generatedDOM);
+      // const menuContainer = document.querySelector(select.containerOf.menu);
+      // menuContainer.appendChild(thisCart.element);
     }
   }
 
